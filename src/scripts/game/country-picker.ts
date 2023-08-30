cdb9e627fc50e2130aa9bd805883776b28ebb600
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2023-08-30 11:55:55 +0300

    Question picker algorithm which I will scrap rn

--- src/scripts/game/country-picker.ts
+++ src/scripts/game/country-picker.ts
@@ -2,39 +2,120 @@ import type { CountryData } from "../lib/countryjson";
 import { shuffleArray } from "../util/math-util";
 import type { CurrentGuess } from "./game-logic";
 
+class Node {
+    // Levels are reversed (bottom is 0, increases with height) as the height changes over time
+    level: number;
+    parent: ParentNode | undefined;
+
+    constructor(level: number) {
+        this.level = level;
+    }
+}
+
+class CountryNode extends Node {
+    country: CountryData;
+
+    constructor(country: CountryData, level: number) {
+        super(level);
+        this.country = country;
+    }
+}
+
+class ParentNode extends Node {
+    children: Node[] = [];
+
+    gameStatus: undefined | number | "Done";
+    falseGuesses = 0;
+
+    constructor(level: number) {
+        super(level);
+    }
+}
+
+const allNodes: Node[][] = [[]];
+
+let activeParent: ParentNode | undefined = undefined;
+
 let askedCountryIndex = 0;
-let countriesToAsk: CountryData[] = [];
-let countriesToAskNext: CountryData[] = [];
+let allCountries: CountryData[] = [];
 
 export function setupPicker(countryMap: Map<string, CountryData>) {
-    countriesToAsk = Array.from(countryMap.values());
+    allCountries = Array.from(countryMap.values());
 
-    shuffleArray(countriesToAsk);
+    shuffleArray(allCountries);
 }
 
 export function pickFirstCountry(): CountryData | undefined {
-    if (countriesToAsk.length === 0) {
+    if (allCountries.length === 0) {
         return undefined;
     }
-    return countriesToAsk[askedCountryIndex++];
+    return allCountries[askedCountryIndex++];
 }
 
 export function pickNewCountry(currentGuess: CurrentGuess): CountryData | undefined {
     const prevCountry = currentGuess.country;
 
-    if (currentGuess.falseGuesses > 0) {
-        countriesToAskNext.push(prevCountry);
+    if (activeParent === undefined) {
+
+        if (currentGuess.falseGuesses <= 0) {
+            return newUnaskedCountry();
+        }
+        return newNodeAndCountry(prevCountry)
+
     }
 
-    if (askedCountryIndex >= countriesToAsk.length) {
-        countriesToAsk = countriesToAskNext;
-        countriesToAskNext = [];
-        askedCountryIndex = 0;
-    }
+    return countryFromActiveParent(currentGuess.falseGuesses <= 0)
+}
 
-    if (countriesToAsk.length === 0 || countriesToAsk.length === 1 && countriesToAsk[0] === prevCountry) {
+function newUnaskedCountry(): CountryData | undefined {
+    if (askedCountryIndex >= allCountries.length) {
+        return undefined;
+    }
+    return allCountries[askedCountryIndex];
+}
+
+function newNodeAndCountry(prevCountry: CountryData) {
+    if (allCountries.length === 0 || allCountries.length === 1 && allCountries[0] === prevCountry) {
         return undefined;
     }
 
-    return countriesToAsk[askedCountryIndex++];
+    const newNode = new CountryNode(prevCountry, 0);
+    allNodes[0].push(newNode);
+
+    const newParent = pairIfPossible(newNode);
+    if (newParent !== undefined) activeParent = newParent;
+
+    return allCountries[askedCountryIndex++];
 }
+
+function countryFromActiveParent(prevWasCorrect: boolean): CountryData | undefined {
+     // The function is called after the if statement that ensures this
+    activeParent = activeParent as ParentNode;
+
+    if (activeParent.gameStatus === undefined) {
+        activeParent.gameStatus = 0;
+        return (activeParent.children[0] as CountryNode).country;
+    }
+
+    if (activeParent.gameStatus === whatever)
+}
+
+function pairIfPossible(node: Node): ParentNode | undefined {
+    const level = node.level;
+    const possiblePair = allNodes[level][allNodes[level].length - 2];
+
+    if (possiblePair === undefined || possiblePair.parent !== undefined) return undefined;
+
+    if (allNodes.length <= level+1) allNodes.push([]);
+
+    const newParent = new ParentNode(level+1);
+    allNodes[newParent.level].push(newParent);
+
+    newParent.children = [node, possiblePair];
+    node.parent = newParent;
+    possiblePair.parent = newParent;
+
+    pairIfPossible(newParent);
+
+    return newParent;
+} 
